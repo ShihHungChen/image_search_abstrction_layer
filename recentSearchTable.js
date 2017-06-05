bbf36af8acdb13c06a980441d2701c7c588c704f
Shi Hung <c.shihhung@gmail.com>
2017-06-05 15:55:14 +0000

    Use mongoose module instead of useing mongodb directly

--- recentSearchTable.js
+++ recentSearchTable.js
@@ -1,46 +1,11 @@
-var mongo = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-function InsertSearchDataIntoDB(searchValue){
-    mongo.connect('mongodb://localhost:27017/imagesearchabstactionlayer', function(err, db){
-        if(err){
-            db.close();
-            return console.error('There was an error : ', err);
-        }
-        else{
-            var collection = db.collection('recentSearch');
-            var data ={value : searchValue, time : new Date()};
-            collection.insert(data, function(err, result){
-                if(err)
-                    console.error('There was an error : ',err);
-                else
-                    console.log('Inserted a search data into recentSearch collection')
-            })
-        }
-        db.close();
-    })
-}
+const searchData = new Schema({
+    searchValue : String,
+    searchDate : Date
+})
 
-function queryRecentSearchDataFromDB(callBack){
-    mongo.connect('mongodb://localhost:27017/imagesearchabstactionlayer', function(err, db){
-        if(err){
-            db.close();
-            return console.error('There was an error : ', err);
-        }
-        else{
-            var collection = db.collection('recentSearch');
-            var queryData = collection.find();
-            console.log('Here');
-            queryData.each(function(err, doc){
-                if(err)
-                    return console.error('There was an error : ', err);
-                else
-                    return console.log(doc);
-            })
-        }
-        db.close();
-    })
-}
+const modelClass = mongoose.model('recent_search', searchData);
 
-
-exports.InsertSearchDataIntoDB = InsertSearchDataIntoDB;
-exports.queryRecentSearchDataFromDB = queryRecentSearchDataFromDB;
+module.exports = modelClass;
--- server.js
+++ server.js
@@ -1,8 +1,45 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const searchSchema = require('./recentSearchTable')
+const url = require('url');
+const app = express();
+const mongoose = require('mongoose');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/imagesearchabstactionlayer/data');
+
+app.get('/api/imagesearch/:SearchValue', function (req, res){
+  var search_value = req.params.SearchValue;
+  var offset_value = req.query.offset;
+  
+  var searchData = new searchSchema({
+    searchValue : search_value,
+    searchDate : new Date()
+  });
+  
+  searchData.save(function(err){
+    if(err){
+      console.error('There was an error: ', err);
+    }
+    else{
+      console.log('Save search data into mongodb success');
+      res.send(searchData);
+    }
+  })
+  
+});
+
+app.get('/api/latest/imagesearch', function(req, res){
+  
+  searchSchema.find().where('recent_search').exec(function(err, data){
+    if(err){
+      console.error('There was an error: ', err);
+    }
+    else{
+      console.log(data);
+    }
+  });
+  
+  res.end();
 })
 
 app.listen(8080, function () {
